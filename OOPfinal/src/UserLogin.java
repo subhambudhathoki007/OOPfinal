0354650e728657e2e30c5281c20f9d23c6b3f46d
Stark Industries <subhambudhathoki007@gmail.com>
2021-09-26 23:18:58 +0545

    third commit

--- OOPfinal/src/UserLogin.java
+++ OOPfinal/src/UserLogin.java
@@ -42,3 +42,29 @@ public class UserLogin {
 	            }
 	        });
 	    }
+	    public UserLogin() {
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setBounds(450, 190, 1014, 597);
+	        setResizable(false);
+	        contentPane = new JPanel();
+	        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	        setContentPane(contentPane);
+	        contentPane.setLayout(null);
+
+	        JLabel lblNewLabel = new JLabel("Login");
+	        lblNewLabel.setForeground(Color.BLACK);
+	        lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+	        lblNewLabel.setBounds(423, 13, 273, 93);
+	        contentPane.add(lblNewLabel);
+	        
+	        textField = new JTextField();
+	        textField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+	        textField.setBounds(481, 170, 281, 68);
+	        contentPane.add(textField);
+	        textField.setColumns(10);
+
+	        passwordField = new JPasswordField();
+	        passwordField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+	        passwordField.setBounds(481, 286, 281, 68);
+	        contentPane.add(passwordField);
+
